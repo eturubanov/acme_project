a8f8a81a5f8e750a6790a61d34fe93b40577faa0
Evgeny Turubanov <eturubanov@yandex.ru>
2023-06-30 15:48:32 +0300

    Валидация формы

--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+
+from birthday.models import Birthday
+
+
+@admin.register(Birthday)
+class Birthday(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'birthday'
+    )
